deee5b5bcbc6b42a65854aef27690e5275961c96
Alun Jones <alunacjones@gmail.com>
2023-07-12 20:26:32 +0100

    add try parse and more tests. also tweak regex

--- LSL.DataUri.Tests/DataUriTests.cs
+++ LSL.DataUri.Tests/DataUriTests.cs
@@ -20,13 +20,14 @@ namespace LSL.DataUri.Tests
         }
 
         [TestCase("data2:a/type;base64,AEWE")]
+        [TestCase("data:a/type;base64,not-base-64!")]
         [TestCase("data:AEWE")]
         public void DateUri_Parse_GivenAnInvalidDataUri_ItShouldThrowAnArgumetnException(string uri)
         {
             using var scope = new AssertionScope();
 
-            new Action(() => DataUri.Parse(uri)).Should().Throw<ArgumentException>();
-        }        
+            new Action(() => DataUri.Parse(uri)).Should().Throw<FormatException>();
+        }
 
         [TestCase(new byte[] { 1, 2, 3 }, "a-type", "data:a-type;base64,AQID")]
         [TestCase(new byte[] { 1, 2, 3 }, "", "data:;base64,AQID")]
@@ -35,6 +36,28 @@ namespace LSL.DataUri.Tests
             using var scope = new AssertionScope();
 
             new DataUri(data, mimeType).ToString().Should().Be(expectedResult);
+        }
+
+        [TestCase("data:a/type;base64,AEWE", "004584", "a/type")]
+        [TestCase("data:base64,AEWE", "004584", "")]
+        public void DateUri_TryParse_GivenAValidDataUri_ItShouldReturnTheExpectedBytes(string uri, string expectedHexString, string expectedContentType)
+        {
+            using var scope = new AssertionScope();
+
+            var succeeded = DataUri.TryParse(uri, out var result);
+
+            Convert.ToHexString(result.Data).Should().Be(expectedHexString);
+            result.MimeType.Should().Be(expectedContentType);
+        }
+
+        [TestCase("data2:a/type;base64,AEWE")]
+        [TestCase("data:a/type;base64,not-base-64!")]
+        [TestCase("data:AEWE")]
+        public void DateUri_TryParse_GivenAnInvalidDataUri_ItShouldThrowAnArgumetnException(string uri)
+        {
+            using var scope = new AssertionScope();
+
+            DataUri.TryParse(uri, out var result).Should().BeFalse();
         }        
     }
 }
--- LSL.DataUri/DataUri.cs
+++ LSL.DataUri/DataUri.cs
@@ -8,7 +8,7 @@ namespace LSL.DataUri
     /// </summary>
     public sealed class DataUri
     {
-        private static Regex PartResolver = new Regex(@"^data:((?<mime>[\w/\-\.]+);)?(?<encoding>\w+),(?<data>.*)");
+        private static Regex PartResolver = new Regex(@"^data:((?<mime>[\w\/\-\.\+]+);)?(?<encoding>\w+),(?<data>.+)$");
 
         /// <summary>
         /// Create a DataUri instance with the given data and mime type
@@ -26,19 +26,26 @@ namespace LSL.DataUri
         /// </summary>
         /// <param name="dataUri"></param>
         /// <returns></returns>
+        /// <exception cref="System.FormatException">Gets thrown if the provide string was not in the correct format</exception>
         public static DataUri Parse(string dataUri)
         {
-            var matchedData = PartResolver.Match(dataUri);
+            var parseResult = InnerTryParse(dataUri, out var result);
 
-            if (!matchedData.Success)
+            if (!parseResult.Success)
             {
-                throw new ArgumentException($"The dataUri must be in base64 format to be parsed ({dataUri})");
+                throw new FormatException(parseResult.Error);
             }
 
-            var regexMatch = PartResolver.Match(dataUri);
-
-            return new DataUri(Convert.FromBase64String(regexMatch.Groups["data"].Value), regexMatch.Groups["mime"].Value);
+            return result;
         }
+
+        /// <summary>
+        /// Try to parse a satring into
+        /// </summary>
+        /// <param name="dataUri"></param>
+        /// <param name="result"></param>
+        /// <returns>true if it worked and also sets the out parameter to the result</returns>
+        public static bool TryParse(string dataUri, out DataUri result) => InnerTryParse(dataUri, out result).Success;
 
         /// <summary>
         /// Provides the byte array of the data URI
@@ -57,5 +64,29 @@ namespace LSL.DataUri
         /// </summary>
         /// <returns></returns>
         public override string ToString() => $"data:{MimeType};base64,{Convert.ToBase64String(Data)}";
+
+        private static (bool Success, string Error) InnerTryParse(string dataUri, out DataUri result)
+        {
+            var regexMatch = PartResolver.Match(dataUri);
+
+            if (!regexMatch.Success)
+            {
+                result = null;
+                return (false, $"The dataUri must be in base64 format to be parsed ({dataUri})");
+            }
+
+            try
+            {
+                var bytes = Convert.FromBase64String(regexMatch.Groups["data"].Value);
+                result = new DataUri(bytes, regexMatch.Groups["mime"].Value);
+
+                return (true, string.Empty);                
+            }
+            catch (FormatException e)
+            {
+                result = null;
+                return (false, e.Message);            
+            }
+        }
     }
 }
